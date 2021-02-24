144b0c15df1afc752d1be936d4160533c57746b1
DohnalMichal <michal.dohnal7@gmail.com>
2021-02-24 14:46:54 +0100

    Change file structure
    Separate file for components

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
-import "./scss/main.scss";
+import "../scss/main.scss";
 import Header from "./Header";
 import Articles from "./Articles";
 import Footer from "./Footer";
